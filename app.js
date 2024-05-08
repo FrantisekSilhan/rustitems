14ee20330d2e5900d420bd88cc6a68b3da6ea59c
František Šilhán <frantisek@slhn.cz>
2024-05-08 17:37:44 +0200

    add dollar symbols

--- app.js
+++ app.js
@@ -432,11 +432,11 @@ app.get("/inventories", (req, res) => {
   
           document.getElementById("data").innerHTML = \`
             <p>Total Bandits Items: \${data.totalBanditsAmount}</p>
-            <p>Total Bandits USD: \${data.totalBanditsUSD || "Error fetching price"}</p>
-            <p>Total Bandits USD (No Fee): \${data.totalBanditsUSDNoFee === 0.01 ? "Error fetching price" : data.totalBanditsUSDNoFee}</p>
+            <p>Total Bandits USD: $\${data.totalBanditsUSD || "Error fetching price"}</p>
+            <p>Total Bandits USD (No Fee): $\${data.totalBanditsUSDNoFee === 0.01 ? "Error fetching price" : data.totalBanditsUSDNoFee}</p>
             <p>Steam Market Supply: \${data.steamMarketSupply}</p>
-            <p>Price For One: \${data.price}</p>
-            <p>Price For One (No Fee): \${data.priceNoFee}</p>
+            <p>Single Item USD: $\${data.price}</p>
+            <p>Single Item USD (No Fee): $\${data.priceNoFee}</p>
             <table border="1">
               <thead>
                 <tr>
